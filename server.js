370c97800aaebf744adb24470a187c98d069b8ad
GauPanisset <gauthier.panisset@depinfonancy.net>
2018-08-16 16:54:18 +0200

    heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 app.use(BP.json());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  req.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
