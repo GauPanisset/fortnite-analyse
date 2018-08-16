7f314c6ad4c1eb6fb68f8d15f07bf80c2aab6f09
GauPanisset <gauthier.panisset@depinfonancy.net>
2018-08-16 21:09:08 +0200

    heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 app.use(BP.json());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Origin", 'https://fortnite-vue.herokuapp.com');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
