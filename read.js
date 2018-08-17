ccd07611c0fb6046e4d57f97a9d3d74c098523d2
GauPanisset <gauthier.panisset@depinfonancy.net>
2018-08-17 09:56:33 +0200

    heroku

--- read.js
+++ read.js
@@ -137,7 +137,7 @@ router.post('/', Storage.upload.single('replay'), (req, res, next) => {
         Fs.unlink(myPath, (err) => {
           if (err) throw err;
           tic = Date.now();
-          console.log("4 : " + tic - toc);
+          console.log("4 : " + (tic - toc));
           console.log('deleted');
         });
 
